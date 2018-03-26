346ad4a46644368ffe68ede2ea29266e6fbd0725
rdfenix <rhoodsk8erboy@gmail.com>
2018-03-26 16:32:55 -0300

    Criando funcao login

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -14,14 +14,43 @@ import {
 const width = Dimensions.get('screen').width
 
 export default class Login extends Component {
+
+    constructor(){
+        super()
+        this.state = {user: '', pass: ''}
+        this.login = this.login.bind(this)
+    }
+
+    login(){
+        const uri = "https://instalura-api.herokuapp.com/api/public/login";
+        const requestInfo = {
+            method: 'POST',
+            body: JSON.stringify({
+                login: this.state.user,
+                senha: this.state.pass
+            }),
+            headers: new Headers({
+                'Content-Type': 'application/json'
+            })
+        };
+
+        fetch(uri, requestInfo)
+        .then(response => {
+            if(response.ok)
+                return response.text()
+            throw new Error("Não foi possivel fazer o login")
+        })
+        .then(token => console.warn(token))
+    }
+
     render(){
         return(
             <View style={styles.container}>
                 <Text style={styles.title}>Instalura</Text>
                 <View style={styles.form}>
-                    <TextInput placeholder="User" style={styles.input} onChangeText={text => this.setState({user: text})} />
-                    <TextInput placeholder="Password" style={styles.input} onChangeText={text => this.setState({pass: text})} />
-                    <Button title="Login" onPress={() => console.warn("Login")} />
+                    <TextInput placeholder="User" style={styles.input} onChangeText={text => this.setState({user: text})} autoCapitalize="none" />
+                    <TextInput placeholder="Password" style={styles.input} onChangeText={text => this.setState({pass: text})} secureTextEntry={true} />
+                    <Button title="Login" onPress={this.login} />
                 </View>
             </View>
         )
