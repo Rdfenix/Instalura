4150157da443cca79d909c95c48c6cc81776cf0f
rdfenix <rhoodsk8erboy@gmail.com>
2018-03-26 15:34:05 -0300

    Criando a tela de login

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -5,17 +5,33 @@ import {
     View,
     Image,
     TouchableOpacity,
-    TextInput
+    TextInput,
+    Platform,
+    Dimensions,
+    Button
   } from 'react-native';
 
+const width = Dimensions.get('screen').width
 
 export default class Login extends Component {
     render(){
         return(
-            <View>
-                <TextInput placeholder="User" onChangeText={text => this.setState({user: text})} />
-                <TextInput placeholder="Password" onChangeText={text => this.setState({pass: text})} />
+            <View style={styles.container}>
+                <Text style={styles.title}>Instalura</Text>
+                <View style={styles.form}>
+                    <TextInput placeholder="User" style={styles.input} onChangeText={text => this.setState({user: text})} />
+                    <TextInput placeholder="Password" style={styles.input} onChangeText={text => this.setState({pass: text})} />
+                    <Button title="Login" onPress={() => console.warn("Login")} />
+                </View>
             </View>
         )
     }
 }
+
+
+const styles = StyleSheet.create({
+    container: {flex: 1, alignItems: 'center', justifyContent: 'center'},
+    title: {fontWeight: 'bold', fontSize: 26,},
+    form: {width: width * 0.8},
+    input: {height: 40, borderBottomColor: '#ddd', borderBottomWidth: 1,}
+})
